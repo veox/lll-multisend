0a8fe817d074e0bcd8314c14f99706d26cd64cc0
Noel Maersk <veox@users.noreply.github.com>
2017-12-11 16:37:30 +0200

    Update source/ABI/test from populus repo: greeting with numbers.

--- tests/test_greeter_lll.py
+++ tests/test_greeter_lll.py
@@ -2,14 +2,14 @@ def test_greeter(chain):
     greeter, _ = chain.provider.get_or_deploy_contract('Greeter')
 
     greeting = greeter.call().greet()
-    assert greeting == 'Hello, lovely!.. <3'
+    assert greeting == 42
 
 
 def test_custom_greeting(chain):
     greeter, _ = chain.provider.get_or_deploy_contract('Greeter')
 
-    set_txn_hash = greeter.transact().setGreeting('Guten Tag')
+    set_txn_hash = greeter.transact().setGreeting(1337)
     chain.wait.for_receipt(set_txn_hash)
 
     greeting = greeter.call().greet()
-    assert greeting == 'Guten Tag'
+    assert greeting == 1337
