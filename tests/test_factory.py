62ebd1d0f519b59d63e766703a69f75521cf3938
Noel Maersk <veox@users.noreply.github.com>
2018-01-04 19:09:02 +0200

    tests: minor refactor in `factory` test.

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -3,17 +3,18 @@ def get_greeter_addr_from_log(chain, receipt):
     addr = chain.web3.toChecksumAddress(logtopic)
     return addr
 
-def test_factory_stamps_greeter(chain):
+def create_greeter(chain, factory):
+    txhash = factory.transact().stamp()
+    txreceipt = chain.wait.for_receipt(txhash)
+
+    greeteraddr = get_greeter_addr_from_log(chain, txreceipt)
+
+    return greeteraddr
+
+def test_factory_creates_greeters(chain):
     factory, _ = chain.provider.get_or_deploy_contract('factory')
 
-    txhash = factory.transact().stamp()
-    txreceipt = chain.wait.for_receipt(txhash)
-    greeter0addr = get_greeter_addr_from_log(chain, txreceipt)
-    print(greeter0addr)
+    greeter0addr = create_greeter(chain, factory)
+    greeter1addr = create_greeter(chain, factory)
 
-    txhash = factory.transact().stamp()
-    txreceipt = chain.wait.for_receipt(txhash)
-    greeter1addr = get_greeter_addr_from_log(chain, txreceipt)
-    print(greeter1addr)
-
-    assert False
+    assert greeter0addr != greeter1addr
