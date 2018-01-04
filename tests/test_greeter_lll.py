7d25c2e15424a66662e6ec3d2d45bd8490ad1100
Noel Maersk <veox@users.noreply.github.com>
2018-01-04 16:02:47 +0200

    contracts: lower-case file names - more lispy.

--- tests/test_greeter_lll.py
+++ tests/test_greeter_lll.py
@@ -1,12 +1,12 @@
 def test_greeter(chain):
-    greeter, _ = chain.provider.get_or_deploy_contract('Greeter')
+    greeter, _ = chain.provider.get_or_deploy_contract('greeter')
 
     greeting = greeter.call().greet()
     assert greeting == 42
 
 
 def test_custom_greeting(chain):
-    greeter, _ = chain.provider.get_or_deploy_contract('Greeter')
+    greeter, _ = chain.provider.get_or_deploy_contract('greeter')
 
     set_txn_hash = greeter.transact().setGreeting(1337)
     chain.wait.for_receipt(set_txn_hash)
