e7b43b3d7d15d262b6e704f21b32e906976c657b
Noel Maersk <veox@users.noreply.github.com>
2018-01-18 19:59:03 +0200

    multisend: passes minimal test, with some guards in contract disabled.

--- tests/test_multisend.py
+++ tests/test_multisend.py
@@ -2,8 +2,21 @@ def test_multisend(chain):
     ms, _ = chain.provider.get_or_deploy_contract('multisend')
 
     to = [
-        '0x0101010101010101010101010101010101010101'
+        '0x0101010101010101010101010101010101010101',
+        '0x0202020202020202020202020202020202020202',
+        '0x0303030303030303030303030303030303030303'
     ]
-    amt = [42]*len(to)
+    amt = list(range(42,42+len(to)))
+
+    assert len(to) == len(amt)
+
     txhash = ms.transact({'value': sum(amt)}).sendMany(to, amt)
-    chain.wait.for_receipt(txhash)
+    txreceipt = chain.wait.for_receipt(txhash)
+
+    print(txreceipt)
+    print(len(txreceipt['logs']))
+
+    assert chain.web3.eth.getBalance(ms.address) == 0
+    assert chain.web3.eth.getBalance(to[0]) == amt[0]
+
+    assert False
