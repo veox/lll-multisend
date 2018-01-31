741f080b7175e770cfe3c8a43a4390c6e25bf508
Noel Maersk <veox@users.noreply.github.com>
2018-01-31 21:12:28 +0200

    cloning-vat: more post-debug clean-up.

--- tests/test_cloning-vat.py
+++ tests/test_cloning-vat.py
@@ -9,15 +9,17 @@ def test_vat(chain):
     txhash = vat0.transact().clone(vat0.address)
     txreceipt = chain.wait.for_receipt(txhash)
 
-    # DEBUG
-    print('-'*33 + 'MEMDMP' + '-'*33)
-    data = txreceipt['logs'][0]['data']
-    data = data[2:] # snip 0x
-    for i in range(int(len(data)/64)):
-        print(chain.web3.toHex(i*32), data[i*64:i*64+64], sep='\t')
+    # DEBUG: uncomment to view memory during execution
+    # print('-'*33 + 'MEMDMP' + '-'*33)
+    # data = txreceipt['logs'][0]['data']
+    # data = data[2:] # snip 0x
+    # for i in range(int(len(data)/64)):
+    #     print(chain.web3.toHex(i*32), data[i*64:i*64+64], sep='\t')
 
     # extract cloned contract's address from log entry
-    vat1address = chain.web3.toChecksumAddress(txreceipt['logs'][1]['data'])
+    vat1address = chain.web3.toChecksumAddress(txreceipt['logs'][0]['data'])
+    # DEBUG: use this instead when viewing memory during execution
+    # vat1address = chain.web3.toChecksumAddress(txreceipt['logs'][1]['data'])
 
     # form an object so the cloned contract can be interacted with
     CloningVat = chain.provider.get_contract_factory('cloning-vat')
@@ -31,7 +33,10 @@ def test_vat(chain):
     txreceipt = chain.wait.for_receipt(txhash)
 
     # form another object, for the twice cloned cloning-vat
-    vat2address = chain.web3.toChecksumAddress(txreceipt['logs'][1]['data'])
+    vat2address = chain.web3.toChecksumAddress(txreceipt['logs'][0]['data'])
+    # DEBUG: use this instead when viewing memory during execution
+    # vat2address = chain.web3.toChecksumAddress(txreceipt['logs'][1]['data'])
+
     vat2 = CloningVat(address=vat2address)
 
     assert vat1.address != vat2.address
