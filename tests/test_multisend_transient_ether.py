fb7ce60cdd81af0045dd434f0c0869aaffc57161
Noel Maersk <veox+gits@veox.pw>
2018-10-30 19:32:43 +0200

    tests/transient: do use constructed `txdata` + other conveniences (test still fails).
    No longer using a hard-coded list of recipients, but instead
    generating a variable-sized one, based on `nrecipients` variable.
    (Should help with benchmarking later on.)
    
    There seems to be an issue with the program itself, though: the
    balance assertion at the end of the test fails. I.e., the
    recipient account's balance does not increase.

--- tests/test_multisend_transient_ether.py
+++ tests/test_multisend_transient_ether.py
@@ -10,24 +10,16 @@ def give_balances(chain, to):
     return
 
 def test_multisend_transient_ether(chain):
-    ms, _ = chain.provider.get_or_deploy_contract('multisend-resident-ether')
-
-    pp(ms)
-
     # FIXME: manual copy-paste from `lllc -o -x multisend-transient-ether.lll`
     mscode = '0x' + '6040356060525b60605115603c5760206060510260a0016040526000806000806020604051013560405135611337f1506001606051036060526006565b00'
 
-    to = [
-        '0x0101010101010101010101010101010101010101',
-        '0x0202020202020202020202020202020202020202',
-        '0x0303030303030303030303030303030303030303',
-        '0x0404040404040404040404040404040404040404',
-        '0x0505050505050505050505050505050505050505',
-        '0x0606060606060606060606060606060606060606',
-    ]
+    nrecipients = 10
+    to = [chain.web3.toHex(
+        chain.web3.toBytes(addr).rjust(20, b'\0')
+    ) for addr in range(4096, 4096+nrecipients)]
     amt = list(range(42, 42 + len(to)))
 
-    assert len(to) == len(amt)
+    assert nrecipients == len(to) == len(amt)
 
     # add zeros on the right (i.e. left-align)
     mscode_bytes = chain.web3.toBytes(hexstr=mscode).ljust(64, b'\0')
@@ -37,33 +29,34 @@ def test_multisend_transient_ether(chain):
     # `to` is already in hex-string, need same for `amt`
     amt_hexstr = [chain.web3.toHex(
         chain.web3.toBytes(val).rjust(32, b'\0')
-        ) for val in amt]
+    ) for val in amt]
 
     # interleave `to` and `amt`, converting to `bytes` in the process
     msdata_bytes = b''.join([chain.web3.toBytes(hexstr=val)
                              for pair in zip(to, amt_hexstr) for val in pair])
 
     # WORKHERE
-    txdata = mscode_bytes + nrecipients_bytes + msdata_bytes
+    txdata_bytes = mscode_bytes + nrecipients_bytes + msdata_bytes
+    txdata = chain.web3.toHex(txdata_bytes)
     pp(txdata)
 
     # seed balances so we don't pay for account creation (skews gas use benchmark)
+    # TODO: use own multisend! much faster to test than making `nrecipients` blocks
     give_balances(chain, to)
 
     # build/send tx
     txhash = chain.web3.eth.sendTransaction({
         'from': chain.web3.eth.coinbase,
         'to': '', # sic!
-        'data': '',
+        'data': txdata,
         'value': sum(amt),
-        'gas': 30000 * len(to), # FIXME: magicnum
+        'gas': 3000 * len(to), # FIXME: magicnum
         })
     txreceipt = chain.wait.for_receipt(txhash)
 
     # pretty when run as `pytest --capture=no <path/to/file>`
     print('')
-    print('Gas used (total):', txreceipt['gasUsed'])
+    print('Gas used (total):   ', txreceipt['gasUsed'])
     print('Gas used (avg/xfer):', txreceipt['gasUsed']/len(to))
 
-    assert chain.web3.eth.getBalance(ms.address) == 0
     assert chain.web3.eth.getBalance(to[0]) >= amt[0]
